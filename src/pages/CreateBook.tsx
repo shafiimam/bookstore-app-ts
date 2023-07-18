db033d1843ec1a591d3bf2018c0afffd04847d5a
Shafi Imam <shafi.imam@springrain.io>
2023-07-18 16:59:54 +0600

    create template

---
+++ src/pages/CreateBook.tsx
@@ -0,0 +1,6 @@
+
+export default function CreateBook() {
+  return (
+    <div>CreateBook</div>
+  )
+}
---
+++ src/pages/EditBook.tsx
@@ -0,0 +1,6 @@
+
+export default function EditBook() {
+  return (
+    <div>EditBook</div>
+  )
+}
